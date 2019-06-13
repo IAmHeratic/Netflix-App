d67c3c09e4a498a7efa8f6cf995add163db94db3
Jose <jrodr41@gmail.com>
2019-06-12 22:18:46 -0500

    Completed Project

---
+++ netflixapp/BusinessTierLogic.cs
@@ -0,0 +1,408 @@
+﻿/*
+ * Jose E. Rodriguez
+ * CS 341 SPring 2018
+ * Project 8 - Netflix Database App - Part 2
+ * University of Illinois at Chicago
+ */
+
+//
+// BusinessTier:  business logic, acting as interface between UI and data store.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace BusinessTier
+{
+
+  //
+  // Business:
+  //
+  public class Business
+  {
+    //
+    // Fields:
+    //
+    private string _DBFile;
+    private DataAccessTier.Data dataTier;
+
+
+    //
+    // Constructor:
+    //
+    public Business(string DatabaseFilename)
+    {
+      _DBFile = DatabaseFilename;
+
+      dataTier = new DataAccessTier.Data(DatabaseFilename);
+    }
+
+
+    //
+    // TestConnection:
+    //
+    // Returns true if we can establish a connection to the database, false if not.
+    //
+    public bool TestConnection()
+    {
+      return dataTier.TestConnection();
+    }
+
+
+    //
+    // GetNamedUser:
+    //
+    // Retrieves User object based on USER NAME; returns null if user is not
+    // found.
+    //
+    // NOTE: there are "named" users from the Users table, and anonymous users
+    // that only exist in the Reviews table.  This function only looks up "named"
+    // users from the Users table.
+    //
+    public User GetNamedUser(string UserName)
+    {
+            // Handle special character '
+            UserName = UserName.Replace("'", "''");
+
+            // SQL string
+            string SQL = string.Format(@"
+            SELECT UserID, Occupation
+            FROM Users
+            WHERE UserName = '" + UserName + @"'");
+
+            // Execute SQL
+            DataSet ds = dataTier.ExecuteNonScalarQuery(SQL);
+            DataTable dt = ds.Tables["TABLE"];
+
+            // Create user object
+            UserName = UserName.Replace("''", "'");
+            int UserID = Convert.ToInt32(dt.Rows[0]["UserID"]);
+            string Occupation = Convert.ToString(dt.Rows[0]["Occupation"]);
+            User u = new User(UserID, UserName, Occupation);
+
+            // return User
+            return u;
+    }
+
+
+
+    public IReadOnlyList<Movie> GetAllMovies()
+    {
+            // SQL string
+            string SQL = (@"
+            SELECT MovieName, MovieID
+            FROM Movies
+            ORDER BY MovieName");
+
+            // Execute SQL
+            DataSet ds = dataTier.ExecuteNonScalarQuery(SQL);
+
+            // Create list of movies
+            Movie m;
+            List<Movie> movies = new List<Movie>();
+
+            foreach (DataRow row in ds.Tables["TABLE"].Rows)
+            {
+                m = new Movie(Convert.ToInt32(row["MovieID"]), Convert.ToString(row["MovieName"]));
+                movies.Add(m);
+            }
+
+            // Return list of movies
+            return movies;
+    }
+
+
+    //
+    // GetAllNamedUsers:
+    //
+    // Returns a list of all the users in the Users table ("named" users), sorted 
+    // by user name.
+    //
+    // NOTE: the database also contains lots of "anonymous" users, which this 
+    // function does not return.
+    //
+    public IReadOnlyList<User> GetAllNamedUsers()
+    {
+            // SQL string
+            string SQL = @"
+            SELECT *
+            FROM Users
+            ORDER BY UserName ASC";
+
+            // Execute SQL
+            DataSet ds = dataTier.ExecuteNonScalarQuery(SQL);
+
+            // Insert all users into list
+            string username, occupation;
+            int user_ID;
+            User u;
+            List<User> users = new List<User>();
+
+            foreach(DataRow row in ds.Tables["TABLE"].Rows)
+            {
+                user_ID = Convert.ToInt32(row["UserID"]);
+                username = Convert.ToString(row["UserName"]);
+                occupation = Convert.ToString(row["Occupation"]);
+                u = new User(user_ID, username, occupation);
+                users.Add(u);
+            }
+
+            // Return list
+            return users;
+    }
+
+
+    //
+    // GetMovie:
+    //
+    // Retrieves Movie object based on MOVIE ID; returns null if movie is not
+    // found.
+    //
+    public Movie GetMovie(int MovieID)
+    {
+            // SQL string
+            string SQL = string.Format(@"
+            SELECT MovieName
+            FROM Movies
+            WHERE MovieID = " + MovieID);
+
+            // Execute string
+            object MovieName = dataTier.ExecuteScalarQuery(SQL);
+
+            // Was object found
+            if (MovieName == null)
+                return null;
+
+            // Movie object
+            Movie M = new Movie(MovieID, MovieName.ToString());
+            return M;
+    }
+
+
+    //
+    // GetMovie:
+    //
+    // Retrieves Movie object based on MOVIE NAME; returns null if movie is not
+    // found.
+    //
+    public Movie GetMovie(string MovieName)
+    {
+            // Handle single quotes in movie names
+            MovieName = MovieName.Replace("'", "''");
+
+            // SQL string
+            string SQL = string.Format(@"
+            SELECT MovieID
+            FROM Movies
+            WHERE MovieName = '" + MovieName + "'");
+
+            // Execute SQL
+            object MovieID = dataTier.ExecuteScalarQuery(SQL);
+
+            // Was object found
+            if (MovieID == null)
+                return null;
+
+            // Movie object
+            MovieName = MovieName.Replace("''", "'");
+            Movie M = new Movie((int)MovieID, MovieName);
+            return M;
+    }
+
+
+    //
+    // AddReview:
+    //
+    // Adds review based on MOVIE ID, returning a Review object containing
+    // the review, review's id, etc.  If the add failed, null is returned.
+    //
+    public Review AddReview(int MovieID, int UserID, int Rating)
+    {
+            // SQL string
+            string SQL = string.Format(@"
+            INSERT INTO Reviews(MovieID, UserID, Rating) VALUES("+ MovieID +", "+ UserID +", "+ Rating + @");
+            SELECT ReviewID
+            FROM Reviews
+            WHERE ReviewID = SCOPE_IDENTITY();");
+
+            // Execute SQL
+            object result = dataTier.ExecuteScalarQuery(SQL);
+            int ReviewID = Convert.ToInt32(result);
+
+            // Create review object
+            Review r = new Review(ReviewID, MovieID, UserID, Rating);
+
+            // return Review object
+            return r;
+    }
+
+
+    //
+    // GetMovieDetail:
+    //
+    // Given a MOVIE ID, returns detailed information about this movie --- all
+    // the reviews, the total number of reviews, average rating, etc.  If the 
+    // movie cannot be found, null is returned.
+    //
+    public MovieDetail GetMovieDetail(int MovieID)
+    {
+            // Get AVG rating, reviews, num reviews, and movie object
+            
+            // SQL string to get reviews
+            string SQL1 = string.Format(@"
+            SELECT ReviewID, MovieID, UserID, Rating
+            FROM Reviews
+            WHERE MovieID = "+ MovieID +@"
+            ORDER BY Rating DESC, UserID ASC");
+
+            // SQL string to get movie name
+            string SQL2 = string.Format(@"
+            SELECT MovieName
+            FROM Movies
+            WHERE MovieID = " + MovieID);
+
+            // Execute SQL
+            DataSet ds = dataTier.ExecuteNonScalarQuery(SQL1);
+            object result = dataTier.ExecuteScalarQuery(SQL2);
+
+            // Loop through table of reviews and count reviews, sum rating
+            Review r;
+            int r_ID, m_ID, u_ID, rating;
+            int num_reviews = 0;
+            double sum_ratings = 0;
+            List<Review> reviews = new List<Review>();
+
+            foreach(DataRow row in ds.Tables["TABLE"].Rows)
+            {
+                r_ID = Convert.ToInt32(row["ReviewID"]);
+                m_ID = Convert.ToInt32(row["MovieID"]);
+                u_ID = Convert.ToInt32(row["UserID"]);
+                rating = Convert.ToInt32(row["Rating"]);
+                num_reviews++;
+                sum_ratings += rating;
+                r = new Review(r_ID, m_ID, u_ID, rating);
+                reviews.Add(r);
+            }
+
+            // Compute Average Rating
+            double avg_rating = 0.0;
+
+            if (num_reviews > 0)
+                avg_rating = sum_ratings / num_reviews;
+
+            // Create movie detail object and return it
+            Movie m = new Movie(MovieID, (string)result);
+            MovieDetail md = new MovieDetail(m, avg_rating, num_reviews, reviews);
+            return md;
+    }
+
+
+    //
+    // GetUserDetail:
+    //
+    // Given a USER ID, returns detailed information about this user --- all
+    // the reviews submitted by this user, the total number of reviews, average 
+    // rating given, etc.  If the user cannot be found, null is returned.
+    //
+    public UserDetail GetUserDetail(int UserID)
+    {
+            // Get all reviews, total # of reviews, 
+
+            // SQL string to get all reviews
+            string SQL1 = string.Format(@"
+            SELECT M.MovieName, R.ReviewID, R.MovieID, R.UserID, R.Rating
+            FROM Reviews R
+            INNER JOIN Movies M ON M.MovieID = R.MovieID
+            WHERE R.UserID = "+ UserID +@"
+            ORDER BY M.MovieName ASC");
+
+            // SQL string to get UserName
+            string SQL2 = string.Format(@"
+            SELECT UserName, Occupation
+            FROM Users
+            WHERE UserID = " + UserID);
+
+            // Execute SQL
+            DataSet ds1 = dataTier.ExecuteNonScalarQuery(SQL1);
+            DataSet ds2 = dataTier.ExecuteNonScalarQuery(SQL2);
+            DataTable dt = ds2.Tables["TABLE"];
+
+            // Create list of reviews, count reviews, and sum ratings
+            Review r;
+            int num_reviews = 0;
+            double sum_ratings = 0.0;
+            int ReviewID, MovieID, u_ID, rating;
+            List<Review> reviews = new List<Review>();
+
+            foreach(DataRow row in ds1.Tables["TABLE"].Rows)
+            {
+                //
+                ReviewID = Convert.ToInt32(row["ReviewID"]);
+                MovieID = Convert.ToInt32(row["MovieID"]);
+                u_ID = Convert.ToInt32(row["UserID"]);
+                rating = Convert.ToInt32(row["Rating"]);
+                num_reviews++;
+                sum_ratings += rating;
+                r = new Review(ReviewID, MovieID, u_ID, rating);
+                reviews.Add(r);
+            }
+
+            // Create User object
+            string UserName = Convert.ToString(dt.Rows[0]["UserName"]);
+            string Occupation = Convert.ToString(dt.Rows[0]["Occupation"]);
+            User u = new User(UserID, UserName, Occupation);
+
+            // Create UserDetail object and return it
+            double avg_rating = num_reviews / sum_ratings;
+            UserDetail ud = new UserDetail(u, avg_rating,num_reviews, reviews);
+            return ud;
+    }
+
+
+    //
+    // GetTopMoviesByAvgRating:
+    //
+    // Returns the top N movies in descending order by average rating.  If two
+    // movies have the same rating, the movies are presented in ascending order
+    // by name.  If N < 1, an EMPTY LIST is returned.
+    //
+    public IReadOnlyList<Movie> GetTopMoviesByAvgRating(int N)
+    {
+            // SQL string
+            string SQL = string.Format(@"
+            SELECT TOP " + N + @" t.MovieName, t.MovieID, AVG(CAST(t.Rating AS DECIMAL(10,2))) AS avg_rating
+            FROM
+            (
+	            SELECT M.MovieName, R.MovieID, R.Rating
+	            FROM Movies M, Reviews R
+	            WHERE M.MovieID = R.MovieID
+            ) t
+            GROUP BY t.MovieName, t.MovieID
+            ORDER BY avg_rating DESC, t.MovieName DESC");
+
+            // Execute SQL
+            DataSet ds = dataTier.ExecuteNonScalarQuery(SQL);
+
+            // For each movie add the movie to list
+            Movie m;
+            int movieID;
+            string movieName;
+            List<Movie> movies = new List<Movie>();
+
+            foreach (DataRow row in ds.Tables["TABLE"].Rows)
+            {
+                movieID = Convert.ToInt32(row["MovieID"]);
+                movieName = Convert.ToString(row["MovieName"]);
+                m = new Movie(movieID, movieName);
+                movies.Add(m);
+            }
+
+            // return list of movies
+            return movies;
+    }
+
+
+  }//class
+}//namespace
---
+++ netflixapp/Form1.cs
@@ -0,0 +1,436 @@
+﻿/*
+ * Jose E. Rodriguez
+ * CS 341 Spring 2018
+ * Project 8 Part 1 - Netflix Database App
+ * University of Illinois at Chicago
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace NetflixApp
+{
+    public partial class Form1 : Form
+    {
+        // Returns the name of the last button pressed
+        private string lastButtonPressed = "None";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Checks that the database exists */
+        private bool fileExists(string filename)
+        {
+            if (!System.IO.File.Exists(filename))
+            {
+                string msg = string.Format("Input file not found: '{0}'",
+                    filename);
+
+                MessageBox.Show(msg);
+                return false;
+            }
+
+            // exists!
+            return true;
+        }
+
+
+        /* Displays Average Rating and Movie ID in text boxes right of listbox */
+        private void getAvgRating_movieID()
+        {
+            int index = this.listBox1.SelectedIndex;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get ID and Average rating for the movie
+            BusinessTier.Movie movie = btier.GetMovie(this.listBox1.Items[index].ToString());
+
+            if (movie == null)
+            {
+                MessageBox.Show("Movie could not be found!");
+            }
+            else
+            {
+                // Movie exists, display ID and AVG rating
+                BusinessTier.MovieDetail m = btier.GetMovieDetail(movie.MovieID);
+                this.textBox2.Text = movie.MovieID.ToString();
+                this.textBox3.Text = m.AvgRating.ToString();
+            }
+
+            // Done
+            this.textBox7.Text = this.listBox1.Items[index].ToString();
+        }
+
+
+        /* Gets the users ID and occupation and inserts into text boxes */
+        private void getUserID_Occupation()
+        {
+            int index = this.listBox1.SelectedIndex;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get user ID and occupation if user exists
+            BusinessTier.User user = btier.GetNamedUser(this.listBox1.Items[index].ToString());
+
+            if(user == null)
+            {
+                MessageBox.Show("User does not exist!");
+                return;
+            }
+            else
+            {
+                // User exists, display their User ID and Occupation, if any
+                this.textBox2.Text = user.UserID.ToString();
+
+                if (string.IsNullOrWhiteSpace(user.Occupation))
+                    this.textBox3.Text = "N/A";
+                else
+                    this.textBox3.Text = user.Occupation.ToString();
+            }
+
+            // Done
+            this.textBox4.Text = this.listBox1.Items[index].ToString();
+        }
+
+
+        /* Increments the appropriate rating counter */
+        private void countRating(ref int[] ratings, int rating)
+        {
+            if (rating == 1)
+                ratings[1]++;
+            else if (rating == 2)
+                ratings[2]++;
+            else if (rating == 3)
+                ratings[3]++;
+            else if (rating == 4)
+                ratings[4]++;
+            else
+                ratings[5]++;
+        }
+
+
+        /* When the user clicks a movie name in the listbox, a different listbox 
+           displays all of its ratings, a third listbox displays statistics about the ratings */
+        private void getAllMovieRatings()
+        {
+            int index = this.listBox1.SelectedIndex;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get the movie and all of it's ratings
+            BusinessTier.Movie movie = btier.GetMovie(this.listBox1.Items[index].ToString());
+
+            if (movie == null)
+            {
+                MessageBox.Show("Movie does not exist!");
+                return;
+            }
+            else
+            {
+                // Display ratings and count ratings
+                int[] ratings = new int[] { 0, 0, 0, 0, 0, 0 };
+                this.listBox2.Items.Clear();
+                BusinessTier.MovieDetail movieDetails = btier.GetMovieDetail(movie.MovieID);
+
+                if (movieDetails.NumReviews == 0)
+                {
+                    this.listBox2.Items.Add("No Reviews");
+                }
+                else
+                {
+                    // For all reviews, get User ID + Rating and insert
+                    this.listBox2.Items.Add(movie.MovieName);
+                    this.listBox2.Items.Add("");
+
+                    foreach (BusinessTier.Review R in movieDetails.Reviews)
+                    {
+                        this.listBox2.Items.Add(R.UserID + ": " + R.Rating);
+                        countRating(ref ratings, R.Rating);
+                    }
+                }
+
+                // Display the rating counts
+                int ratingsTotal = 0;
+                this.listBox3.Items.Clear();
+                this.listBox3.Items.Add(movie.MovieName);
+                this.listBox3.Items.Add("");
+
+                for (int i = 1; i <= 5; ++i)
+                {
+                    this.listBox3.Items.Add(i + ":" + ratings[i]);
+                    ratingsTotal += ratings[i];
+                }
+
+                this.listBox3.Items.Add("");
+                this.listBox3.Items.Add("Total: " + ratingsTotal);
+            }
+        }
+        
+
+        /* Displays all of the users reviews in a listbox */
+        public void getAllUserReviews()
+        {
+            int index = this.listBox1.SelectedIndex;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get user if the user exists
+            BusinessTier.User user = btier.GetNamedUser(this.listBox1.Items[index].ToString());
+
+            if (user == null)
+            {
+                MessageBox.Show("User does not exist!");
+                return;
+            }
+            else
+            {
+                // Get all of the users reviews
+                this.listBox2.Items.Clear();
+                BusinessTier.UserDetail userDetails = btier.GetUserDetail(user.UserID);
+
+                if (userDetails.NumReviews == 0)
+                {
+                    this.listBox2.Items.Add("No Reviews");
+                }
+                else
+                {
+                    // For all reviews, get movie name + rating and insert
+                    this.listBox2.Items.Add(user.UserName);
+                    this.listBox2.Items.Add("");
+
+                    foreach (BusinessTier.Review U in userDetails.Reviews)
+                    {
+                        BusinessTier.Movie m = btier.GetMovie(U.MovieID);
+                        this.listBox2.Items.Add(m.MovieName + "->" + U.Rating);
+                    }
+                }
+            }
+        }
+
+
+        /* List box where user selects a movie and avg rating + movie ID are returned */
+        private void listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            if(this.lastButtonPressed == "All Movies")
+            {
+                getAvgRating_movieID();
+                getAllMovieRatings();
+            } else if (this.lastButtonPressed == "All Users")
+            {
+                getUserID_Occupation();
+                getAllUserReviews();
+            }
+
+            this.Cursor = Cursors.Default;
+        }
+
+
+        /* Movies button that retrieves all movies and inserts into listbox */
+        private void all_movies_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get list of movies
+            IReadOnlyList < BusinessTier.Movie > movies = btier.GetAllMovies();
+
+            // Clear all data in list and text boxes
+            this.listBox1.Items.Clear();
+            this.listBox2.Items.Clear();
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
+
+            // Insert movie names in list box
+            foreach (BusinessTier.Movie M in movies)
+            {
+                listBox1.Items.Add(M.MovieName);
+            }
+
+            // Change labels
+            this.label1.Text = "Movie ID";
+            this.label2.Text = "AVG Rating";
+            this.label3.Text = "Movie Ratings";
+
+            // Done
+            this.lastButtonPressed = "All Movies";
+            //this.listBox1.SelectedIndex = 0;
+            this.Cursor = Cursors.Default;
+        }
+
+
+        /* Lists all the users in the listbox*/
+        private void all_users_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if(!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Retrieve all users and insert into listbox
+            IReadOnlyList< BusinessTier.User > userList = btier.GetAllNamedUsers();
+
+            // Clear all data in list and text boxes
+            this.listBox1.Items.Clear();
+            this.listBox2.Items.Clear();
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
+
+            // Insert all usersnames
+            foreach(BusinessTier.User U in userList)
+            {
+                this.listBox1.Items.Add(U.UserName);
+            }
+
+            // Change labels
+            this.label1.Text = "User ID";
+            this.label2.Text = "Occupation";
+            this.label3.Text = "User's Ratings";
+
+            // Done
+            this.lastButtonPressed = "All Users";
+            this.Cursor = Cursors.Default;
+        }
+
+
+        /* When this button is clicked, display the top N movies */
+        private void topNMovies_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            // test connection to database
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Check that textbox has valid input
+            if(string.IsNullOrWhiteSpace(this.textBox6.Text))
+            {
+                MessageBox.Show("Please enter a value in the textbox!");
+                return;
+            }
+
+            int N = Convert.ToInt32(this.textBox6.Text);
+
+            if (N < 1)
+            {
+                MessageBox.Show("Please enter a number greater than 0!");
+                return;
+            }
+
+            // Get top N movies
+            this.listBox1.Items.Clear();
+            this.Cursor = Cursors.WaitCursor;
+            IReadOnlyList<BusinessTier.Movie> topNMovies = btier.GetTopMoviesByAvgRating(N);
+
+
+            // Insert the top N movies
+            foreach(BusinessTier.Movie M in topNMovies)
+            {
+                BusinessTier.MovieDetail movieDetail = btier.GetMovieDetail(M.MovieID);
+                this.listBox1.Items.Add(M.MovieName + ":" + movieDetail.AvgRating);
+            }
+
+            // Done
+            this.Cursor = Cursors.Default;
+            this.lastButtonPressed = "Top N Movies";
+        }
+
+
+        /* Inserts a rating into the database */
+        private void insert_movieRating_Click(object sender, EventArgs e)
+        {
+            // Check that a movie was selected
+            if (string.IsNullOrWhiteSpace(this.textBox7.Text))
+            {
+                MessageBox.Show("Please select a Movie from the list!");
+                return;
+            }
+
+            // Check that a Username was selected
+            if (string.IsNullOrWhiteSpace(this.textBox4.Text))
+            {
+                MessageBox.Show("Please select a Username from the list!");
+                return;
+            }
+
+            // Check that Rating is valid
+            int rating = 0;
+            string s = this.textBox5.Text;
+            bool validRating = int.TryParse(s, out rating);
+
+            if (!(validRating && (rating <= 5) && (rating >= 1)))
+            {
+                MessageBox.Show("Please enter a rating between 1 and 5!");
+                return;
+            }
+
+            // test connection to database
+            BusinessTier.Business btier = new BusinessTier.Business(this.textBox1.Text);
+
+            if (!btier.TestConnection())
+            {
+                MessageBox.Show("Can't connect to the database!");
+                return;
+            }
+
+            // Get movie ID and user ID
+            BusinessTier.Movie M = btier.GetMovie(this.textBox7.Text);
+            BusinessTier.User U = btier.GetNamedUser(this.textBox4.Text);
+
+            // Insert review
+            btier.AddReview(M.MovieID, U.UserID, rating);
+            MessageBox.Show("Review was succesfully inserted!");
+        }
+    }
+}
